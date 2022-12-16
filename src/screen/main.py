b01bd7ba719ef0cd1caae22abfa82f5fa3d69f9d
Kenny Carsey <kcarsey@livevox.com>
2022-12-16 10:20:35 -0500

    tab test

--- src/screen/main.py
+++ src/screen/main.py
@@ -13,7 +13,7 @@ layout3 = [[sg.Text('This is layout 3 - It is all Radio Buttons')],
 
 # ----------- Create actual layout using Columns and a row of Buttons
 layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')],
-          [sg.Button('Cycle Layout'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
+          [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
 
 window = sg.Window('Swapping the contents of a window', layout, no_titlebar=True, location=(0,0), size=(800,480), finalize=True) #keep_on_top=True to keep on top
 window.maximize()
@@ -34,3 +34,4 @@ while True:
         layout = int(event)
         window[f'-COL{layout}-'].update(visible=True)
 window.close()
+
