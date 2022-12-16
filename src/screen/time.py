2f4e649b75f637564d62e3986aeda86f3b4b348f
Kenny Carsey <kcarsey@livevox.com>
2022-12-16 10:25:11 -0500

    working clock

--- src/screen/time.py
+++ src/screen/time.py
@@ -14,10 +14,17 @@ tab2_layout = [[sg.Text('This is tab 2')],
 tab3_layout = [[sg.Text('This is tab 3')],
                [sg.Button('Button 3')]]
 
-# Create the tab layout using sg.TabGroup
-tab_layout = [sg.TabGroup([[sg.Tab('Tab 1', tab1_layout),
-                           sg.Tab('Tab 2', tab2_layout),
-                           sg.Tab('Tab 3', tab3_layout)]])]
+# Create the tab group
+tab_group = sg.TabGroup([
+    [sg.Tab('Tab 1', tab1_layout)],
+    [sg.Tab('Tab 2', tab2_layout)],
+    [sg.Tab('Tab 3', tab3_layout)],
+], size=(800, 480), tab_location='bottomleft')
+
+# Create the window layout
+tab_layout = [
+    [tab_group],
+]
 
 # Create the main window using sg.Window
 window = sg.Window('Window Title', layout=tab_layout, size=(800,480))
@@ -28,8 +35,7 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     elif event == '__TIMEOUT__':
-        # Update the clock text every 100 milliseconds
         now = datetime.datetime.now()
-        window['-CLOCK-'].update(now.strftime('%Y-%m-%d %H:%M:%S'))
+        window['-CLOCK-'].update(now.strftime('%H:%M:%S'))
 
 window.close()
