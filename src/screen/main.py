2ecb37f1fe2d7617abb6cc56452f08c658089fd2
Kenny Carsey <kcarsey@livevox.com>
2022-12-15 23:49:15 -0500

    Update main.py

--- src/screen/main.py
+++ src/screen/main.py
@@ -15,8 +15,8 @@ layout3 = [[sg.Text('This is layout 3 - It is all Radio Buttons')],
 layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')],
           [sg.Button('Cycle Layout'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
 
-window = sg.Window('Swapping the contents of a window', layout, no_titlebar=True, location=(0,0), size=(800,480)) #keep_on_top=True to keep on top
-
+window = sg.Window('Swapping the contents of a window', layout, no_titlebar=True, location=(0,0), size=(800,480), finalize=True) #keep_on_top=True to keep on top
+window.maximize()
 
 
 layout = 1  # The currently visible layout
