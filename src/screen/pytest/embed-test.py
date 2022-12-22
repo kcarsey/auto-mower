63065c3f55a4b848210a1f0637b2d59d54591e51
Kenny Carsey <ryan4602@gmail.com>
2022-12-22 00:04:32 -0500

    testing tkmapview

--- src/screen/pytest/embed-test.py
+++ src/screen/pytest/embed-test.py
@@ -1,7 +1,7 @@
 #<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d24790.277856716584!2d-82.13013938115233!3d39.043027350019734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1671314973302!5m2!1sen!2sus" width="640" height="480" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 import PySimpleGUI as sg
-from PySimpleGUI import Webview
+import webview
 
 sg.theme('DarkAmber')  # Add a touch of color
 
