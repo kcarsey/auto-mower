0f6262cb833d4783a155a05f09345fb1b67aa309
Kenny Carsey <ryan4602@gmail.com>
2023-09-05 21:23:18 -0400

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,59 +1,127 @@
+import sys
 import tkinter
-import tkintermapview
-
-# create tkinter window
-root_tk = tkinter.Tk()
-root_tk.geometry(f"{1000}x{700}")
-root_tk.title("map_view_polygon_example.py")
-root_tk.attributes("-fullscreen", True)
-
-# create map widget
-map_widget = tkintermapview.TkinterMapView(root_tk, width=1024, height=600, corner_radius=0)
-map_widget.pack(fill="both", expand=True)
+import tkinter.messagebox
+from tkintermapview import TkinterMapView
 
 
-def polygon_click(polygon):
-    print(f"polygon clicked - text: {polygon.name}")
+class App(tkinter.Tk):
+
+    APP_NAME = "map_view_demo.py"
+    WIDTH = 800
+    HEIGHT = 750
+
+    def __init__(self, *args, **kwargs):
+        tkinter.Tk.__init__(self, *args, **kwargs)
+        
+        self.title(self.APP_NAME)
+        self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.bind("<Return>", self.search)
+
+        if sys.platform == "darwin":
+            self.bind("<Command-q>", self.on_closing)
+            self.bind("<Command-w>", self.on_closing)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(2, weight=0)
+        self.grid_rowconfigure(1, weight=1)
+
+        self.search_bar = tkinter.Entry(self, width=50)
+        self.search_bar.grid(row=0, column=0, pady=10, padx=10, sticky="we")
+        self.search_bar.focus()
+
+        self.search_bar_button = tkinter.Button(master=self, width=8, text="Search", command=self.search)
+        self.search_bar_button.grid(row=0, column=1, pady=10, padx=10)
+
+        self.search_bar_clear = tkinter.Button(master=self, width=8, text="Clear", command=self.clear)
+        self.search_bar_clear.grid(row=0, column=2, pady=10, padx=10)
+
+        self.map_widget = TkinterMapView(width=self.WIDTH, height=600, corner_radius=0)
+        self.map_widget.grid(row=1, column=0, columnspan=3, sticky="nsew")
+        self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
+
+        self.marker_list_box = tkinter.Listbox(self, height=8)
+        self.marker_list_box.grid(row=2, column=0, columnspan=1, sticky="ew", padx=10, pady=10)
+
+        self.listbox_button_frame = tkinter.Frame(master=self)
+        self.listbox_button_frame.grid(row=2, column=1, sticky="nsew", columnspan=2)
+
+        self.listbox_button_frame.grid_columnconfigure(0, weight=1)
+
+        self.save_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="save current marker",
+                                                 command=self.save_marker)
+        self.save_marker_button.grid(row=0, column=0, pady=10, padx=10)
+
+        self.clear_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="clear marker list",
+                                                  command=self.clear_marker_list)
+        self.clear_marker_button.grid(row=1, column=0, pady=10, padx=10)
+
+        self.connect_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="connect marker with path",
+                                                    command=self.connect_marker)
+        self.connect_marker_button.grid(row=2, column=0, pady=10, padx=10)
+
+        self.map_widget.set_address("NYC")
+
+        self.marker_list = []
+        self.marker_path = None
+
+        self.search_marker = None
+        self.search_in_progress = False
+
+    def search(self, event=None):
+        if not self.search_in_progress:
+            self.search_in_progress = True
+            if self.search_marker not in self.marker_list:
+                self.map_widget.delete(self.search_marker)
+
+            address = self.search_bar.get()
+            self.search_marker = self.map_widget.set_address(address, marker=True)
+            if self.search_marker is False:
+                # address was invalid (return value is False)
+                self.search_marker = None
+            self.search_in_progress = False
+
+    def save_marker(self):
+        if self.search_marker is not None:
+            self.marker_list_box.insert(tkinter.END, f" {len(self.marker_list)}. {self.search_marker.text} ")
+            self.marker_list_box.see(tkinter.END)
+            self.marker_list.append(self.search_marker)
+
+    def clear_marker_list(self):
+        for marker in self.marker_list:
+            self.map_widget.delete(marker)
+
+        self.marker_list_box.delete(0, tkinter.END)
+        self.marker_list.clear()
+        self.connect_marker()
+
+    def connect_marker(self):
+        print(self.marker_list)
+        position_list = []
+
+        for marker in self.marker_list:
+            position_list.append(marker.position)
+
+        if self.marker_path is not None:
+            self.map_widget.delete(self.marker_path)
+
+        if len(position_list) > 0:
+            self.marker_path = self.map_widget.set_path(position_list)
+
+    def clear(self):
+        self.search_bar.delete(0, last=tkinter.END)
+        self.map_widget.delete(self.search_marker)
+
+    def on_closing(self, event=0):
+        self.destroy()
+        exit()
+
+    def start(self):
+        self.mainloop()
 
 
-switzerland_marker = map_widget.set_address("Switzerland", marker=True, text="Switzerland")
-map_widget.set_zoom(8)
-
-polygon_1 = map_widget.set_polygon([(46.0732306, 6.0095215),
-                                    (46.3393433, 6.2072754),
-                                    (46.5890691, 6.1083984),
-                                    (46.7624431, 6.4270020),
-                                    (47.2717751, 7.0312500),
-                                    (47.4726629, 6.9982910),
-                                    (47.4057853, 7.3718262),
-                                    (47.5468716, 7.9650879),
-                                    (47.5691138, 8.4045410),
-                                    (47.7540980, 8.6242676),
-                                    (47.5691138, 9.4482422),
-                                    (47.1897125, 9.5581055),
-                                    (46.9352609, 9.8327637),
-                                    (46.9727564, 10.4150391),
-                                    (46.6418940, 10.4479980),
-                                    (46.4605655, 10.0744629),
-                                    (46.2786312, 10.1513672),
-                                    (46.3469276, 9.5581055),
-                                    (46.4454275, 9.3493652),
-                                    (45.8211434, 8.9538574),
-                                    (46.1037088, 8.6352539),
-                                    (46.3696741, 8.3496094),
-                                    (45.9740604, 7.9321289),
-                                    (45.8900082, 7.0971680),
-                                    (46.1417827, 6.8664551),
-                                    (46.4151388, 6.7236328),
-                                    (46.3772542, 6.4160156)],
-                                   fill_color=None,
-                                   outline_color="red",
-                                   border_width=2,
-                                   command=polygon_click,
-                                   name="switzerland_polygon")
-
-# polygon_1.remove_position(46.3772542, 6.4160156)
-# polygon_1.add_position(0, 0, index=5)
-# polygon_1.delete()
-
-root_tk.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.start()
