33ccc597d3f516cb4d1884267bf0d08325d914a7
Kenny Carsey <ryan4602@gmail.com>
2023-09-08 21:38:38 -0400

    working map view in frame-example2

--- src/frame-example2.py
+++ src/frame-example2.py
@@ -2,7 +2,8 @@ import sys
 import tkinter
 import tkinter.messagebox
 from tkinter import font as tkfont
-from tkintermapview import TkinterMapView 
+from tkintermapview import TkinterMapView
+import tkintermapview 
 
 
 class SampleApp(tkinter.Tk):
@@ -84,15 +85,48 @@ class PageTwo(tkinter.Frame):
         button.pack()
 
 class MapView(tkinter.Frame):
+ 
     def __init__(self, parent, controller):
         tkinter.Frame.__init__(self, parent)
         self.controller = controller
-        # label = tkinter.Label(self, text="This is the map", font=controller.title_font)
-        # label.pack(side="bottom", fill="x", pady=10)
-        button = tkinter.Button(self, justify="left", text="Go to the start page",
+        
+        button = tkinter.Button(self, text="Go to the start page",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
+        
+        map_widget = tkintermapview.TkinterMapView(self)
+        map_widget.pack(fill="both", expand=True)
+        map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)  # google satellite
 
+        # set current position and zoom
+        # map_widget.set_position(52.516268, 13.377695, marker=False)  # Berlin, Germany
+        # map_widget.set_zoom(17)
+
+        # set current position with address
+        # map_widget.set_address("Berlin Germany", marker=False)
+
+        def marker_click(marker):
+            print(f"marker clicked - text: {marker.text}  position: {marker.position}")
+
+        # set a position marker (also with a custom color and command on click)
+        marker_2 = map_widget.set_marker(52.516268, 13.377695, text="Brandenburger Tor", command=marker_click)
+        marker_3 = map_widget.set_marker(52.55, 13.4, text="52.55, 13.4")
+        # marker_3.set_position(...)
+        # marker_3.set_text(...)
+        # marker_3.delete()
+
+        # set a path
+        path_1 = map_widget.set_path([marker_2.position, marker_3.position, (52.568, 13.4), (52.569, 13.35)])
+        # path_1.add_position(...)
+        # path_1.remove_position(...)
+        # path_1.delete()
+
+
+
+
+
+
+       
 
 if __name__ == "__main__":
     app = SampleApp()
